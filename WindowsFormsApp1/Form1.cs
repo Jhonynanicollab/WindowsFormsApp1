f413f21ef8ef3503ba9943a37f23d07431cc614f
Jhonynanicollab <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>
2025-06-14 21:52:59 -0500

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Este es el evento del botón
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Crear una instancia del segundo formulario
+            Form2 nuevaVentana = new Form2();
+
+            // Mostrarla
+            nuevaVentana.Show();
+
+            // (Opcional) Ocultar esta ventana actual:
+            // this.Hide();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
